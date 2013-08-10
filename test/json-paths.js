268fabc58bc28ca787b950d10b4fb2fbaf7bbe3e
Eugene Ware <eugene@noblesamurai.com>
2013-08-11 01:36:18 +1000

    path traversal

--- test/json-paths.js
+++ test/json-paths.js
@@ -12,13 +12,32 @@ describe('json paths', function() {
       tags: [ 'tag1', 'tag2', 'tag3' ]
     };
 
-    var paths = jsonPaths([], obj, function (err, slices) {
+    var paths = jsonPaths.slice([], obj, function (err, slices) {
       if (err) return done(err);
       var paths = slices.map(function (data) {
         return data.key.join('.');
       });
-      console.log(paths);
+      expect(paths).to.deep.equals(
+        [ 'name', 'car.make', 'car.model', 'tags.0', 'tags.1', 'tags.2' ]);
+      done();
     });
+  });
+
+  it('should be able to traverse a path', function(done) {
+    var obj = {
+      name: 'Bob',
+      car: {
+        make: 'Toyota',
+        model: 'Camry'
+      },
+      tags: [ 'tag1', 'tag2', 'tag3' ]
+    };
+
+    var path = ['tags', 1];
+
+    var prop = jsonPaths.walk(obj, path);
+    expect(prop).to.equal('tag2');
+
     done();
   });
 });
