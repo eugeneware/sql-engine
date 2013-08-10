8ffa2e440595780541e27daafbc56aedb7b5a3c2
Eugene Ware <eugene@noblesamurai.com>
2013-08-10 23:54:32 +1000

    server proof of concept working

---
+++ server.js
@@ -0,0 +1,35 @@
+var expect = require('chai').expect,
+    mysql = require('mysql2'),
+    levelup = require('levelup'),
+    rimraf = require('rimraf'),
+    levelQuery = require('level-queryengine'),
+    sqlDriver = require('./lib/sql-driver'),
+    jsonqueryEngine = require('jsonquery-engine'),
+    path = require('path');
+
+var testData, db, dbPath = path.join(__dirname, 'data', 'testdb');
+rimraf.sync(dbPath);
+db = levelup(dbPath, { valueEncoding: 'json' });
+db = levelQuery(db);
+testData = [
+  { name: 'Eugene', num: 42 },
+  { name: 'Susan', num: 43 },
+  { name: 'Edmund', num: 88 }
+];
+
+function batchData() {
+  return testData.map(function (data, i) {
+    return {
+      key: i,
+      value: data,
+      type: 'put'
+    };
+  });
+}
+
+var server;
+var users = levelQuery(db.sublevel('users'));
+users.query.use(jsonqueryEngine());
+users.batch(batchData(), function () {
+  server = sqlDriver(db, 3307, undefined, undefined);
+});
